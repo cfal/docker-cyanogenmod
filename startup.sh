c5f84edab8daa14e7afd94b942ec6ed4c71eeaa3
Alex Lau <alau@windows95.local>
2016-04-20 22:18:57 -0700

    Set ccache size to 20gb

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 # Initialize ccache if needed
 if [ ! -f /srv/ccache/CACHEDIR.TAG ]; then
 	echo "Initializing ccache in /srv/ccache..."
-	CCACHE_DIR=/srv/ccache ccache -M 50G
+	CCACHE_DIR=/srv/ccache ccache -M 20G
 fi
 
 export USER="cm"
